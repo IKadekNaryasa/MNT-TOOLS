87e97bef16cf96b96fc0b46401a644d5fef59a1a
IKadekNaryasa <qy.naryasa@gmail.com>
2025-03-17 17:27:17 +0800

    perbaiki fitur cetak qr code

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -60,7 +60,9 @@ class Autoload extends AutoloadConfig
      *
      * @var array<string, string>
      */
-    public $classmap = [];
+    public $classmap = [
+        'TCPDF' => ROOTPATH . 'vendor/tecnickcom/tcpdf/tcpdf.php',
+    ];
 
     /**
      * -------------------------------------------------------------------
